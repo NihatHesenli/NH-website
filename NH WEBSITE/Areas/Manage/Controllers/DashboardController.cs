5df03cb7bb8cc30e5935bad8ea4ee8af1b155603
NihatHesenli <tu7FUKRPS@code.edu.az>
2024-05-12 14:14:21 -0700

    abc

---
+++ NH WEBSITE/Areas/Manage/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NH_WEBSITE.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NH WEBSITE/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NH_WEBSITE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
